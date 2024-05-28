923e5c5605ac9c3c2b6d86cb8d6b4c6ef84a3b3c
Øystein Håberg <islandstone89@gmail.com>
2024-05-29 01:07:03 +0200

    * Added errorMessages in JS * Experimented with required attribute in HTML * Experimented with custom Checkbox * Added color change on label when input is active

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
 /* Select the contact form */
 let form = document.querySelector(".js-contact-form");
+
+/* Select error messages */
+let errorMessages = document.querySelectorAll(".js-error");
+
+form.addEventListener("submit", test);
+
+function test(e) {
+    e.preventDefault();
+    errorMessages.forEach((error) => {{
+        error.style.display = "block";
+    }})
+}
