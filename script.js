89b92c5c66833ec2ddbd1e9563a88cfe8608767a
Øystein Håberg <islandstone89@gmail.com>
2024-05-31 02:00:43 +0200

    * Added success-message in JS * Removed required attribute in HTML * Styled radio buttons and checkbox

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let form = document.querySelector(".js-contact-form");
 /* Select error messages */
 let errorMessages = document.querySelectorAll(".js-error");
 
+/* Select success message  */
+let successMessage = document.querySelector(".success-message");
+
 form.addEventListener("submit", test);
 
 function test(e) {
@@ -12,4 +15,5 @@ function test(e) {
     errorMessages.forEach((error) => {{
         error.style.display = "block";
     }})
+    successMessage.style.display = "block";
 }
